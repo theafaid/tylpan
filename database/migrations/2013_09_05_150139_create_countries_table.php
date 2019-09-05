489a9daec0875f41c473d18d493131d5a913a811
Abdurrhman <abdurrhmanfaid@gmail.com>
2019-09-05 17:06:16 +0200

    Npm install, Initial users, countries tables

---
+++ database/migrations/2013_09_05_150139_create_countries_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCountriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('countries', function (Blueprint $table) {
+            $table->smallIncrements('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('countries');
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,25 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('middle_name')->nullable();
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('gender', ['male', 'female']);
+            $table->unsignedSmallInteger('country_id')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->string('age');
+            $table->string('avatar')->nullable();
+            $table->text('social_links')->nullable();
+            $table->enum('role', ['user', 'delegate', 'admin', 'super_admin']);
+            $table->boolean('active')->default(1);
             $table->rememberToken();
             $table->timestamps();
+
+            $table->foreign('country_id')->references('id')->on('countries')
+                ->onDelete('set null');
         });
     }
 
